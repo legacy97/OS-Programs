1980a32898e7c67527255402270773c1b88231c0
legacy97 <dharshansrinivas.cs24@bmsce.ac.in>
2026-05-29 10:02:43 +0530

    Add files via upload

---
+++ first fit.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main()
+{
+    int blockSize[20], processSize[20];
+    int allocation[20];
+    int m, n, i, j;
+
+    // Input number of memory blocks
+    printf("Enter number of memory blocks: ");
+    scanf("%d", &m);
+
+    // Input sizes of memory blocks
+    printf("Enter sizes of memory blocks:\n");
+    for(i = 0; i < m; i++)
+    {
+        scanf("%d", &blockSize[i]);
+    }
+
+    // Input number of processes
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    // Input sizes of processes
+    printf("Enter sizes of processes:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &processSize[i]);
+    }
+
+    // Initially no allocation
+    for(i = 0; i < n; i++)
+    {
+        allocation[i] = -1;
+    }
+
+    // First-Fit Allocation
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < m; j++)
+        {
+            // Check if block can fit the process
+            if(blockSize[j] >= processSize[i])
+            {
+                allocation[i] = j;
+
+                // Reduce available memory in block
+                blockSize[j] -= processSize[i];
+
+                break;
+            }
+        }
+    }
+
+    // Display allocation details
+    printf("\nProcess No.\tProcess Size\tBlock No.\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d\t\t%d\t\t", i + 1, processSize[i]);
+
+        if(allocation[i] != -1)
+        {
+            printf("%d\n", allocation[i] + 1);
+        }
+        else
+        {
+            printf("Not Allocated\n");
+        }
+    }
+
+    return 0;
+}
